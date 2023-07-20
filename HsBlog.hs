2110e21d9a58379875ed4142052e9b254669b994
Karel Prochazka <karelprochazka2003@gmail.com>
2023-07-20 14:19:03 +0200

    Markdown type

--- HsBlog.hs
+++ HsBlog.hs
@@ -1,5 +1,35 @@
-module HsBlog
-where
+data MarkdownElem =
+    Text String
+    | Heading Int MarkdownElem
+    | Paragraph MarkdownElem
+    | Bold MarkdownElem
+    | Italic MarkdownElem
+    | BoldAndItalic MarkdownElem
+    | Blockquote MarkdownElem
+    | OrderedList [MarkdownElem]
+    | UnorderedList [MarkdownElem]
+    | Code MarkdownElem
+    | CodeBlock [MarkdownElem]
+    | HorizontalRule
+    | Link MarkdownElem String
+    | LinkTitle MarkdownElem String String
+    | Image String String String
+
+
+data Markdown = Markdown [MarkdownElem]
+
+parseMd :: String -> Markdown
+parseMd = undefined
+
+renderAsHtml :: Markdown -> String
+renderAsHtml = undefined
 
 main :: IO ()
-main = putStrLn "<html><head></head><body>Hello there</body></html>"
+main = do
+    filename <- getLine
+    text <- readFile filename
+    let res = renderAsHtml $ parseMd text
+    putStrLn res
+    pure ()
+
+
