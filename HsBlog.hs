fbe1b8aa779c008e45e56fddfd45f87b8f6ec5f4
KaPr3 <karelprochazka2003@gmail.com>
2023-07-25 12:26:04 +0200

    First html rendering

--- HsBlog.hs
+++ HsBlog.hs
@@ -1,3 +1,5 @@
+import GHC.Utils.Misc (count)
+
 data MarkdownElem =
     Text String
     | Heading Int String
@@ -5,6 +7,7 @@ data MarkdownElem =
     | Bold MarkdownElem
     | Italic MarkdownElem
     | BoldAndItalic MarkdownElem
+    | Strikethrough MarkdownElem
     | Blockquote MarkdownElem
     | OrderedList [MarkdownElem]
     | UnorderedList [MarkdownElem]
@@ -18,6 +21,7 @@ data MarkdownElem =
 
 
 data Markdown = Markdown [MarkdownElem]
+    deriving (Show)
 
 data MarkdownContext =
     Empty
@@ -31,24 +35,39 @@ ltrim :: String -> String
 ltrim = after ' '
 
 parseMd :: String -> Markdown
-parseMd = Markdown $ reverse $ parseMdState Empty (lines text) []
+parseMd text = Markdown $ reverse $ parseMdState Empty (lines text) []
 
 parseMdState :: MarkdownContext -> [String] -> [MarkdownElem] -> [MarkdownElem]
-parseMdState Empty (('#':line):text) parsed = parseMdState Empty text ((Heading trimmed number) : parsed) where
+parseMdState Empty (('#':line):text) parsed = parseMdState Empty text ((Heading number trimmed) : parsed) where
     trimmed = ltrim $ after '#' line
     number = count (\c -> c == '#') line + 1
 parseMdState Empty ("":text) parsed = parseMdState Empty text parsed
-parseMdState Empty (line:text) parsed = parseMdState (ParagraphC [Text line]) text parsed
+parseMdState Empty (line:text) parsed = parseMdState (ParagraphC [parseLine line]) text parsed
 parseMdState (ParagraphC p) ("":text) parsed = parseMdState Empty text ((Paragraph p) : parsed)
-parseMdState (ParagraphC p) (line:text) parsed = parseMdState (ParagraphC (p ++ [Text line])) text parsed
+parseMdState (ParagraphC p) (line:text) parsed = parseMdState (ParagraphC (p ++ [parseLine line])) text parsed
 parseMdState (ParagraphC p) [] parsed =  (Paragraph p) : parsed
 parseMdState _ [] parsed = parsed
 
+parseLine :: String -> MarkdownElem
+parseLine = Text -- TODO
+
 renderAsHtml :: Markdown -> String
-renderAsHtml = undefined
+renderAsHtml (Markdown md) = concat $ map renderElem md
+
+el :: String -> String -> String
+el e text = "<" ++ e ++ ">" ++ text ++ "</" ++ e ++ ">"
+
+renderElem :: MarkdownElem -> String
+renderElem (Text text) = text
+renderElem (Heading n text) = el ("h" ++ num) text where num = show n
+renderElem (Paragraph xs) = el "p" $ concat $ map renderElem xs
+renderElem (Bold element) = el "b" $ renderElem element
+renderElem (Italic element) = el "em" $ renderElem element
+renderElem (BoldAndItalic element) = el "em" $ el "b" $ renderElem element
+renderElem (Strikethrough element) = el "strike" $ renderElem element -- TODO: lookup actual html
 
 convert :: String -> String
-convert = renderHtml . parseMd
+convert = renderAsHtml . parseMd
 
 main :: IO ()
 main = do
