249e90edb30bcf1c22b5463956b551214ca4c772
Karel Prochazka <karelprochazka2003@gmail.com>
2023-08-09 19:53:26 +0200

    Bold text and minor changes

--- HsBlog.hs
+++ HsBlog.hs
@@ -5,30 +5,12 @@ import Text.Parsec.String
 import Text.Parsec.Error
 import Text.Parsec.Pos
 
-import Data.Functor.Identity
+import Data.Char (isSpace)
+import Data.List (dropWhileEnd, singleton)
 
-{-
-data MarkdownElem =
-    Text String
-    | Headings [(Int, String)] --
-    | Paragraph [MarkdownElem] --
-    | Bold MarkdownElem
-    | Italic MarkdownElem
-    | BoldAndItalic MarkdownElem
-    | Strikethrough MarkdownElem
-    | Blockquote [MarkdownElem] --
-    | OrderedList [MarkdownElem] --
-    | UnorderedList [MarkdownElem] --
-    | OrderedListNum [MarkdownElem] Int --
-    | UnorderedListNum [MarkdownElem] Int --
-    | Code MarkdownElem
-    | CodeBlock [MarkdownElem] --
-    | HorizontalRule
-    | Link MarkdownElem String
-    | LinkTitle MarkdownElem String String
-    | Image String String String
-    deriving (Show)
--}
+import Control.Monad (join)
+
+import Data.Functor.Identity
 
 data Tag =
     Heading
@@ -45,28 +27,24 @@ data Tag =
     | LineBreak
     | Link
     | Image
+    | Container
+    | Lined
     deriving (Eq, Show)    
 
 data Element =
     Text String
+    | Line String
     | ListElem Tag [Element]
     | SingleElem Tag
-    | NumberElem Tag [Element] Int
-    | RepeatedApplication Tag [Element] Int
-    | String1Elem Tag [Element] String
-    | String2Elem Tag [Element] String String
+    | NumberElem Tag Int [Element]
+    | RepeatedApplication Tag Int [Element]
+    | String1Elem Tag String [Element]
+    | String2Elem Tag String String [Element]
     | String3 Tag String String String
-    deriving (Show)
+    deriving (Eq, Show)
 
 data Markdown = Markdown [Element]
-    deriving (Show)
-
-{-
-data MarkdownContext =
-    EmptyC
-    | ParagraphC [MarkdownElem]
-    | BlockquoteC
--}
+    deriving (Eq, Show)
 
 type ParseResult = Either ParseError Element
 
@@ -77,10 +55,21 @@ after _ [] = []
 ltrim :: String -> String
 ltrim = after ' '
 
+rtrim :: String -> String
+rtrim = dropWhileEnd isSpace
+
+trim :: String -> String
+trim = ltrim . rtrim
+
 count_indent :: String -> Int
 count_indent xs = (num_spaces + 4 * num_tabs) `div` 4 where
     num_spaces = length $ filter (== ' ') xs
     num_tabs = length $ filter (== '\t') xs
+
+endsWith :: String -> String -> Bool
+endsWith [] [] = True
+endsWith [] _ = False
+endsWith (x:xs) y = ((x:xs) == y) || endsWith xs y
 
 escape :: String -> String
 escape = concat . (map escapeChar)
@@ -92,48 +81,31 @@ escape = concat . (map escapeChar)
         escapeChar '\'' = "&#39;"
         escapeChar x = [x]
 
+errorMessage :: String -> String -> ParseResult
+errorMessage tag1 tag2 = Left $ newErrorMessage (Message msg) (initialPos "Inside") where
+    msg = "Can't connect different tags: " ++ tag1 ++ " : " ++ tag2
+
 connect :: Element -> Element -> ParseResult
 connect a (Text "") = Right a
 connect (Text "") a = Right a
 connect (Text a) (Text b) = Right $ Text (a ++ b)
+connect (Text a) (Line b) = Right $ Line (a ++ b)
+connect (ListElem Lined xs) (ListElem Lined ys) = Right $ ListElem Container [ListElem Lined xs, ListElem Lined ys] 
 connect (ListElem tag1 xs) (ListElem tag2 ys) = 
     if tag1 == tag2
         then Right $ ListElem tag1 (xs ++ ys)
-        else Left $ newErrorMessage (Message msg) (initialPos "Inside") where
-            msg = "Can't connect different tags: " ++ (show tag1) ++ (show xs) ++ " : " ++ (show tag2) ++ (show ys)
+        else errorMessage ((show tag1) ++ (show xs)) ((show tag2) ++ (show ys))
 connect (ListElem tag xs) (Text a) = Right $ ListElem tag (xs ++ [Text a])
 connect (Text a) (ListElem tag xs) = Right $ ListElem tag ((Text a) : xs)
-connect (NumberElem tag1 xs n) (NumberElem tag2 ys m) =
+connect (NumberElem tag1 n xs) (NumberElem tag2 m ys) =
     if tag1 == tag2
-        then Right $ ListElem tag1 [NumberElem tag1 xs n, NumberElem tag2 ys m]
-        else Left $ newErrorMessage (Message msg) (initialPos "Inside") where
-            msg = "Can't connect different tags: " ++ (show tag1) ++ (show xs) ++ " : " ++ (show tag2) ++ (show ys)
+        then Right $ ListElem tag1 [NumberElem tag1 n xs, NumberElem tag2 m ys]
+        else errorMessage ((show tag1) ++ (show xs)) ((show tag2) ++ (show ys))
 connect a (ListElem tag xs) = Right $ ListElem tag (a : xs)
 connect (ListElem tag xs) a = Right $ ListElem tag (xs ++ [a])
-{-
-connect (Headings xs) (Headings ys) = Right $ Headings (xs ++ ys)
-connect (Blockquote a) (Blockquote b) = Right $ Blockquote (a ++ b)
-connect (OrderedList a) (OrderedList b) = Right $ OrderedList (a ++ b)
-connect (UnorderedList a) (UnorderedList b) = Right $ UnorderedList (a ++ b)
-connect (CodeBlock a) (CodeBlock b) = Right $ CodeBlock (a ++ b)
-connect (Paragraph a) (Paragraph b) = Right $ Paragraph (a ++ b)
-
-connect (Headings a) (Text b) = Right $ Headings (a ++ [(1, b)])
-connect (Text a) (Headings b)  = Right $ Headings ((1, a) : b)
-connect (Blockquote a) (Text b) = Right $ Blockquote (a ++ [Text b])
-connect (Text a) (Blockquote b) = Right $ Blockquote ((Text a) : b)
-connect (OrderedList a) (Text b) = Right $ OrderedList (a ++ [Text b])
-connect (Text a) (OrderedList b) = Right $ OrderedList ((Text a) : b)
-connect (UnorderedList a) (Text b) = Right $ UnorderedList (a ++ [Text b])
-connect (Text a) (UnorderedList b) = Right $ UnorderedList ((Text a) : b)
-connect (CodeBlock a) (Text b) = Right $ CodeBlock (a ++ [Text b])
-connect (Text a) (CodeBlock b) = Right $ CodeBlock ((Text a) : b)
-connect (Paragraph a) (Text b) = Right $ Paragraph (a ++ [Text b])
-connect (Text a) (Paragraph b) = Right $ Paragraph ((Text a) : b)
--}
-connect a b = Left $ newErrorMessage (Message msg) (initialPos "Inside") where
-    msg = "Error when connecting parsed elements: " ++ (show a) ++ " : " ++ (show b)
-
+connect a b = Right $ ListElem Container [a, b]
+--connect a b = Left $ newErrorMessage (Message msg) (initialPos "Inside") where
+--    msg = "Error when connecting parsed elements: " ++ (show a) ++ " : " ++ (show b)
 
 connectE :: ParseResult -> ParseResult -> ParseResult
 connectE (Left a) _ = Left a
@@ -145,7 +117,7 @@ headingParser = do
     hashes <- many1 $ char '#'
     spaces
     name <- many anyChar
-    pure $ NumberElem Heading [Text name] (length hashes)
+    pure $ NumberElem Heading (length hashes) [Text name] 
 
 blockquoteParser :: Parser Element
 blockquoteParser = do
@@ -161,7 +133,7 @@ oListParser = do
     _ <- char '.'
     spaces
     text <- many anyChar
-    pure $ RepeatedApplication OrderedList [Text text] (count_indent indentation)
+    pure $ RepeatedApplication OrderedList (count_indent indentation) [Text text]
 
 uListParser :: Parser Element
 uListParser = do
@@ -170,7 +142,7 @@ uListParser = do
     _ <- oneOf "-*+"
     spaces
     text <- many anyChar
-    pure $ RepeatedApplication UnorderedList [Text text] (count_indent indentation)
+    pure $ RepeatedApplication UnorderedList (count_indent indentation) [Text text]
 
 codeBlockParser :: Parser Element
 codeBlockParser = do
@@ -187,7 +159,7 @@ horizontalParser = do
 paragraphParser :: Parser Element
 paragraphParser = do
     text <- many anyChar
-    pure $ ListElem Paragraph $ map Text $ lines text
+    pure $ ListElem Paragraph [Line text]
 
 lineParser :: Parser Element
 lineParser = ((try headingParser)
@@ -198,10 +170,28 @@ lineParser = ((try headingParser)
                 <|> (try codeBlockParser)
                 <|> paragraphParser)
 
-wordParser :: Parser Element
-wordParser = undefined --TODO
+boldParser :: Parser Element
+boldParser = do
+    text1 <- many (noneOf "*_")
+    _ <- count 2 (oneOf "*_")
+    text2 <- many (noneOf "*_")
+    _ <- count 2 (oneOf "*_")
+    text3 <- many (noneOf "*_")
+    pure $ ListElem Container [Text text1, ListElem Bold [Text text2], Text text3]
 
+allParser :: Parser Element
+allParser = do
+    text <- many anyChar
+    pure $ Text text
 
+wordParser :: Parser Element --TODO
+wordParser = (try boldParser)
+                <|> allParser
+
+addBr :: Element -> [Element]
+addBr (Text text) = if endsWith text "  " then [Text (trim text), SingleElem LineBreak] else [Text text]
+addBr (ListElem tag xs) = [ListElem tag (concat $ map addBr xs)]
+addBr x = [x]
 
 concatMembers :: Element -> [Element] -> [Element]
 concatMembers (ListElem tag1 xs) ((ListElem tag2 ys) : zs) =
@@ -215,14 +205,15 @@ concatListElem (ListElem tag xs) = ListElem tag $ map concatListElem $ foldr con
 concatListElem x = x
 
 applyTag :: Element -> Element
-applyTag (RepeatedApplication tag xs 0) = ListElem tag xs
-applyTag (RepeatedApplication tag xs n) = ListElem tag [applyTag newelem]
-    where newelem = RepeatedApplication tag xs (n - 1)
+applyTag (RepeatedApplication tag 0 xs) = ListElem tag xs
+applyTag (RepeatedApplication tag n xs) = ListElem tag [applyTag newelem]
+    where newelem = RepeatedApplication tag (n - 1) xs
 applyTag x = x
 
 modifyElement :: Element -> Element
-modifyElement (RepeatedApplication tag xs n) = modifyElement $ applyTag (RepeatedApplication tag xs n)
-modifyElement (ListElem tag xs) = concatListElem $ ListElem tag xs
+modifyElement (RepeatedApplication tag n xs) = modifyElement $ applyTag (RepeatedApplication tag n xs)
+modifyElement (ListElem Paragraph xs) = ListElem Paragraph $ concat $ map addBr xs
+--modifyElement (ListElem tag xs) = concatListElem $ ListElem tag xs
 modifyElement x = x
 
 blocks :: [String] -> [[String]]
@@ -238,27 +229,24 @@ parseMd :: String -> Either ParseError Markdown
 parseMd text = fmap Markdown (sequence $ map parseBlock (blocks $ lines text))
 
 parseBlock :: [String] -> ParseResult
-parseBlock = (fmap modifyElement) . (foldr connectE (Right (Text ""))) . (map parseLine) -- 
+parseBlock = (fmap modifyElement) . (foldr connectE (Right (Text ""))) . (map parseLine)
 
-{-
-parseWords :: MarkdownElem -> MarkdownElem
-parseWords (String x) = parse wordParser ""
-parseWords (Paragraph xs) = Paragraph $ map parseWords xs
-parseWords (Bold x) = Bold $ parseWords x
-parseWords (Italic x) = Italic $ parseWords x
-parseWords (BoldAndItalic x) = BoldAndItalic $ parseWords x
-parseWords (Strikethrough x) = Strikethrough $ parseWords x
-parseWords (Blockquote xs) = Blockquote $ map parseWords xs
-parseWords (OrderedList xs) = OrderedList $ map parseWords xs
-parseWords (UnorderedList xs) = UnorderedList $ map parseWords xs
-parseWords (Link x y) = Link (parseWords x) y
-parseWords (LinkTitle x y z) = LinkTitle (parseWords x) y z
--}
+parseList :: [Element] -> Either ParseError [Element]
+parseList xs = sequence $ (map parseWords xs)
 
-parseWords x = x
+parseWords :: Element -> ParseResult
+parseWords (Text text) = parse wordParser "" text
+parseWords (Line text) = (ListElem Lined) <$> singleton <$> (parse wordParser "" text)
+parseWords (ListElem tag xs) = (ListElem tag) <$> (parseList xs)
+parseWords (SingleElem tag) = Right $ SingleElem tag
+parseWords (NumberElem tag n xs) = (NumberElem tag n) <$> (parseList xs)
+parseWords (RepeatedApplication tag n xs) = (RepeatedApplication tag n) <$> (parseList xs)
+parseWords (String1Elem tag s xs) = (String1Elem tag s) <$> (parseList xs)
+parseWords (String2Elem tag s1 s2 xs) = (String2Elem tag s1 s2) <$> (parseList xs)
+parseWords (String3 tag s1 s2 s3) = Right $ String3 tag s1 s2 s3
 
 parseLine :: String -> ParseResult
-parseLine line = parseWords <$> modifyElement <$> parse lineParser "" line
+parseLine line = join $ parseWords <$> modifyElement <$> parse lineParser "" line
 
 renderAsHtml :: Markdown -> String
 renderAsHtml (Markdown md) = htmlHead $ concat $ map renderElem md
@@ -267,7 +255,7 @@ htmlHead :: String -> String
 htmlHead x = el "html" (el "head" "" ++ el "body" x)
 
 el :: String -> String -> String
-el e text = "<" ++ e ++ ">" ++ text ++ "</" ++ e ++ ">" ++ "\n"
+el e text = "<" ++ e ++ ">" ++ text ++ "</" ++ e ++ ">"
 
 renderHeading :: (Int, String) -> String
 renderHeading (n, text) = el ("h" ++ num) text where num = show n
@@ -295,9 +283,13 @@ renderTag HorizontalRule = "hr"
 renderTag LineBreak = "br"
 renderTag Link = "a"
 renderTag Image = "img"
+renderTag Container = ""
 
 renderElem :: Element -> String
 renderElem (Text text) = text
+renderElem (Line text) = text ++ "\n"
+renderElem (ListElem Lined xs) = (concat $ map renderElem xs) ++ "\n"
+renderElem (ListElem Container xs) = concat $ map renderElem xs
 renderElem (ListElem Code xs) = el "code" $ escape $ concat $ map ((++ "\n") . renderElem) xs
 renderElem (ListElem tag xs) =
     if tag == OrderedList || tag == UnorderedList
@@ -305,25 +297,11 @@ renderElem (ListElem tag xs) =
         else el (renderTag tag) $ concat $ map renderElem xs
 --renderElem (ListElem tag xs) = el (renderTag tag) $ concat $ map renderElem xs
 renderElem (SingleElem tag) = "<" ++ renderTag tag ++ ">"
-renderElem (NumberElem tag xs n) = el t $ concat $ map renderElem xs where t = renderTag tag ++ show n
-renderElem (RepeatedApplication tag xs n) = renderElem $ applyTag $ RepeatedApplication tag xs n
-renderElem (String1Elem tag xs s1) = undefined -- TODO
-renderElem (String2Elem tag xs s1 s2) = undefined -- TODO
+renderElem (NumberElem tag n xs) = el t $ concat $ map renderElem xs where t = renderTag tag ++ show n
+renderElem (RepeatedApplication tag n xs) = renderElem $ applyTag $ RepeatedApplication tag n xs
+renderElem (String1Elem tag s1 xs) = undefined -- TODO
+renderElem (String2Elem tag s1 s2 xs) = undefined -- TODO
 renderElem (String3 tag s1 s2 s3) = undefined -- TODO
-
-{-
-renderElem (Headings xs) = concat (map renderHeading xs)
-renderElem (Paragraph xs) = el "p" $ concat $ map renderElem xs
-renderElem (Bold element) = el "b" $ renderElem element
-renderElem (Italic element) = el "em" $ renderElem element
-renderElem (BoldAndItalic element) = el "em" $ el "b" $ renderElem element
-renderElem (Strikethrough element) = el "s" $ renderElem element
-renderElem (Blockquote xs) = el "blockquote" $ concat $ map renderElem xs
-renderElem (OrderedList xs) = el "ol" $ renderList xs
-renderElem (UnorderedList xs) = el "ul" $ renderList xs
-renderElem (CodeBlock xs) = el "pre" $ el "code" $ escape $ concat $ map ((++ "\n") . renderElem) xs
-renderElem HorizontalRule = "<hr>"
--}
 
 convert :: String -> Either ParseError String
 convert text = renderAsHtml <$> parseMd text
@@ -338,7 +316,8 @@ main = do
     let filename = "example copy.md" -- TODO: remove later
     text <- readFile filename
     let md = parseMd text -- TODO: remove later
-    print md -- TODO: remove later
+    print (map (map parseLine) (blocks $ lines text))
+    --print md -- TODO: remove later
     let res = convert text
     writeIfRight res "first.html"
 
